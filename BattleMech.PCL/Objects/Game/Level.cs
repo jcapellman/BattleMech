561321a8e0b04a2fd889776c9e5976636282985b
Jarred Capellman <jcapellman@hotmail.com>
2015-09-27 17:57:31 -0400

    Added: Initial Objects for Level functionality

---
+++ BattleMech.PCL/Objects/Game/Level.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BattleMech.PCL.Objects.Game {
+    public class Level {
+        public List<LevelObject> Objects { get; set; }
+        
+        public string LevelName { get; set; } 
+    }
+}
---
+++ BattleMech.PCL/Objects/Game/LevelObject.cs
@@ -0,0 +1,13 @@
+﻿using BattleMech.PCL.Enums;
+
+namespace BattleMech.PCL.Objects.Game {
+    public class LevelObject {
+        public string TextureName { get; set; }
+
+        public int PositionX { get; set; }
+
+        public int PositionY { get; set; }
+
+        public TEXTURABLE_ITEM_TYPES Type { get; set; }
+    }
+}
