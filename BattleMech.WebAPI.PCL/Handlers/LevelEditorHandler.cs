24ebe1c6c272363baaa766129fa0858ad7c5fea9
jcapellman <jcapellman@hotmail.com>
2015-10-09 17:17:15 -0400

    Added: LavelEditorHandler and LevelEditorController for use with the Level Editor functionality

---
+++ BattleMech.WebAPI.PCL/Handlers/LevelEditorHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+using BattleMech.WebAPI.PCL.Transports.Common;
+using BattleMech.WebAPI.PCL.Transports.Internal;
+using BattleMech.WebAPI.PCL.Transports.Levels;
+
+namespace BattleMech.WebAPI.PCL.Handlers {
+    public class LevelEditorHandler : BaseHandler {
+        public LevelEditorHandler(HandlerItem handlerItem) : base(handlerItem, "LevelEditor") { }
+
+        public async Task<CTI<LevelEditorResponseItem>> GetLevelEditorData() { return await GET<CTI<LevelEditorResponseItem>>(string.Empty); }
+    }
+}
---
+++ BattleMech.WebAPI/Controllers/LevelEditorController.cs
@@ -0,0 +1,9 @@
+﻿using BattleMech.WebAPI.Managers;
+using BattleMech.WebAPI.PCL.Transports.Common;
+using BattleMech.WebAPI.PCL.Transports.Levels;
+
+namespace BattleMech.WebAPI.Controllers {
+    public class LevelEditorController : BaseController {
+        public CTI<LevelEditorResponseItem> GET() { return new LevelManager(AUTH_USER).GetEditorData(); }
+    }
+}
