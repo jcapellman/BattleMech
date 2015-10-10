76e48a756e61178f95fd8133c8814263501369f1
Jarred Capellman <jcapellman@hotmail.com>
2015-10-10 11:13:59 -0400

    Added: LevelObjectLite and LevelResponseItem class objects

---
+++ BattleMech.PCL/Objects/Game/LevelObjectLite.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace BattleMech.PCL.Objects.Game {
+    [DataContract]
+    public class LevelObjectLite {
+        [DataMember]
+        public int AssetID { get; set; }
+        
+        [DataMember]
+        public int PositionX { get; set; }
+
+        [DataMember]
+        public int PositionY { get; set; }
+    }
+}
---
+++ BattleMech.WebAPI.PCL/Transports/Levels/LevelResponseItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace BattleMech.WebAPI.PCL.Transports.Levels {
+    [DataContract]
+    public class LevelResponseItem {
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string LevelData { get; set; }
+    }
+}
