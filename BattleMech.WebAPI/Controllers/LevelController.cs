c91e54e14c9c876c10c40af5deacf430293bf8e1
Jarred Capellman <jcapellman@hotmail.com>
2015-10-10 11:13:29 -0400

    Added: GetLevel method in the LevelController of the WebAPI Service

--- BattleMech.WebAPI/Controllers/LevelController.cs
+++ BattleMech.WebAPI/Controllers/LevelController.cs
@@ -3,9 +3,12 @@
 using BattleMech.DataLayer.PCL.Views.Levels;
 using BattleMech.WebAPI.Managers;
 using BattleMech.WebAPI.PCL.Transports.Common;
+using BattleMech.WebAPI.PCL.Transports.Levels;
 
 namespace BattleMech.WebAPI.Controllers {
     public class LevelController : BaseController {
-        public CTI<List<LevelListingView>> GET() { return new LevelManager(AUTH_USER).GetLevelListing(); }        
+        public CTI<List<LevelListingView>> GET() { return new LevelManager(AUTH_USER).GetLevelListing(); }
+        
+        public CTI<LevelResponseItem> GET(int levelID) { return new LevelManager(AUTH_USER).GetLevel(levelID); }      
     }
 }
