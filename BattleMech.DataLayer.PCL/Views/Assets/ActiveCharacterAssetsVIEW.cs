689e6b578fa5d46aa914a9d2a652b244abab9f47
Jarred Capellman <jcapellman@hotmail.com>
2015-10-10 20:41:27 -0400

    Added: ActiveCharacterAssetsVIEW for pulling the associated AssetID for the player's character

---
+++ BattleMech.DataLayer.PCL/Views/Assets/ActiveCharacterAssetsVIEW.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace BattleMech.DataLayer.PCL.Views.Assets {
+    [DataContract]
+    public class ActiveCharacterAssetsVIEW {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public int UserID { get; set; }
+
+        [DataMember]
+        public int AssetID { get; set; }
+    }
+}
--- BattleMech.DataLayer/Contexts/AssetContext.cs
+++ BattleMech.DataLayer/Contexts/AssetContext.cs
@@ -5,5 +5,7 @@ using Microsoft.Data.Entity;
 namespace BattleMech.DataLayer.Contexts {
     public class AssetContext : BaseContext {
         public DbSet<ActiveAssetsVIEW> AssetsDS { get; set; }
+
+        public DbSet<ActiveCharacterAssetsVIEW> CharacterAssetsVIEWDS { get; set; } 
     }
 }
